23add522c1701c70c23deb513decadc5c93cd565
Ryan Minh-Tri Le <ryanle2003@g0001.stanford.edu>
2025-01-29 22:51:34 -0800

    working forward pass

--- PCGNode.py
+++ PCGNode.py
@@ -7,6 +7,9 @@ from parallel_ops.parallel_ops import parallel_ops
 from algebraic_ops import algebraic_ops
 from node_status import node_status
 from node_types import node_types
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 class PCGNode:
     def __init__(self, id, type, parents, machine_mapping=None, operation=None, dim=None, data=None) -> None:
@@ -23,19 +26,16 @@ class PCGNode:
         if self.type == node_types.INPUT:
             return
         if self.type == node_types.OUTPUT:
-            if len(self.parents) == 0:
-                return
-            self.data = self.parents[0].data
             return
         
         if self.operation == parallel_ops.PARTITION:
-            self.data = partition_tensor(self.data[0], self.machine_mapping, self.dim)
+            self.data = partition_tensor(self.data[0][0], self.machine_mapping, self.dim)
         elif self.operation == parallel_ops.COMBINE:
-            self.data = combine_tensors(self.data, self.machine_mapping, self.dim)
+            self.data = combine_tensors(self.data[0], self.machine_mapping, self.dim)
         elif self.operation == parallel_ops.REPLICATE:
-            self.data = replicate_tensor(self.data[0], self.machine_mapping)
+            self.data = replicate_tensor(self.data[0][0], self.machine_mapping)
         elif self.operation == parallel_ops.REDUCE:
-            self.data = reduce_tensors(self.data, self.machine_mapping)
+            self.data = reduce_tensors(self.data[0], self.machine_mapping)
         elif self.operation == algebraic_ops.MATMUL:
-            self.data = [x @ y for x, y in zip(self.parents[0].data, self.parents[1].data)]
+            self.data = [x @ y for x, y in zip(self.data[0], self.data[1])]
         
--- training.py
+++ training.py
@@ -1,12 +1,18 @@
 from concurrent.futures import ThreadPoolExecutor
 from node_status import node_status
 import logging
+import threading
+import os
+from node_types import node_types
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 def execute_pcg(pcg):
-    logging.debug("Executing PCG")
+    logging.info("Executing PCG")
+    cpu_count = os.cpu_count()
+    print(f"Available CPU cores: {cpu_count}")
+
     remaining_parents = {}
     for id, node in pcg.items():
         remaining_parents[id] = len(node.parents)
@@ -21,24 +27,28 @@ def execute_pcg(pcg):
             
             for future in futures:
                 future.result()
+    
+    
 
 
 def process_node(node, pcg, remaining_parents):
-    logging.debug(f"Processing node: {node.id}, status: {node.status}")
+    logging.info(f"Processing node: {node.id}, status: {node.status}, thread: {threading.get_ident()}")
+
+    if node.type == node_types.OUTPUT:
+        logging.info(f"Output node: {node.id}, data: {node.data}")
 
     node.status = node_status.RUNNING
     node.forward_pass()
     node.status = node_status.COMPLETED
 
-    logging.debug(f"Completed node: {node.id}, status: {node.status}")
+    logging.info(f"Completed node: {node.id}, status: {node.status}")
 
     for child in [n for n in pcg if node.id in pcg[n].parents]:
-
-        for item in node.data:
-            pcg[child].data.append(item)
-
         remaining_parents[child] -= 1
         if remaining_parents[child] == 0:
+            for parent in pcg[child].parents:
+                pcg[child].data.append(pcg[parent].data)
+
             pcg[child].status = node_status.READY
 
     

d3cb272ba1ecc52790eb3fd18f144d2c8e23d9a5
Ryan <ryanmtle@gmail.com>
2025-03-11 16:22:46 -0700

    dtensor testing

---
+++ dtensor.py
@@ -0,0 +1,54 @@
+import torch
+print(torch.__version__)
+import torch.distributed as dist
+from torch.distributed.device_mesh import init_device_mesh
+from torch.distributed._tensor import DTensor
+from torch.distributed._tensor import Shard, Replicate
+
+import os
+
+local_rank = int(os.environ.get("LOCAL_RANK", 0))
+torch.cuda.set_device(local_rank)
+
+torch.distributed.init_process_group(backend="nccl")
+mesh = init_device_mesh("cuda", (2, 2), mesh_dim_names=("dp", "tp"))
+
+local_weight = torch.randn(4, 2, device="cuda", requires_grad=True)
+local_input = torch.randn(1, 4, device="cuda")
+
+weight_dt = DTensor.from_local(local_weight, mesh, [Shard(0), Replicate()])
+weight_dt.requires_grad
+input_dt = DTensor.from_local(local_input, mesh, [Replicate(), Shard(1)])
+
+output_dt = torch.matmul(input_dt, weight_dt)
+local_target = torch.randn_like(output_dt.to_local())
+target_dt = DTensor.from_local(local_target, mesh, output_dt.placements)
+
+learning_rate = 0.01
+
+print(f"Before Weight: {weight_dt}")
+for epoch in range(10):
+    if local_weight.grad is not None:
+        local_weight.grad.zero_()
+
+    # Forward pass
+    output_dt = torch.matmul(input_dt, weight_dt)
+    
+    # compute loss
+    loss_dt = ((output_dt - target_dt) ** 2).mean()
+    loss = loss_dt.to_local()
+    
+    # Backward pass
+    loss.backward()
+    
+    # Manual gradient update (simplified)
+    with torch.no_grad():
+        local_weight.data -= learning_rate * local_weight.grad
+        # Update the DTensor with the new weights
+        # weight_dt = DTensor.from_local(local_weight, mesh, [Shard(0), Replicate()])
+        # weight_dt.requires_grad
+    
+    # Print progress
+    
+    print(f"Loss: {loss.item()}")
+print(f"After Weight: {weight_dt}")

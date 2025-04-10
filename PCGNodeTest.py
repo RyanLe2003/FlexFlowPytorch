2da876f91b6ba937a16a570b6b934532d4b6834d
Ryan <ryanmtle@gmail.com>
2025-04-10 13:10:08 -0700

    working autograds for all ops

--- PCGNodeTest.py
+++ PCGNodeTest.py
@@ -1,56 +1,121 @@
-from PCGNode import Partition
+from PCGNode import Partition, Replicate, Combine, Reduce
 import torch.nn as nn
 import torch
 from concurrent.futures import ThreadPoolExecutor
 
-class testModel(nn.Module):
+class testModelWPartition(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.weight = nn.Parameter(torch.rand((4, 4)))
     
     @staticmethod
     def multiply(a, b):
-        res = a * b
-        print(res)
-        res = res.to("cuda:0")
+        res = torch.matmul(a, b)
         return res
 
     
     def forward(self, x):
-        parts = Partition.apply(x, ["cuda:0", "cuda:1", "cuda:2", "cuda:3"], 0)
-        parts2 = Partition.apply(self.weight, ["cuda:0", "cuda:1", "cuda:2", "cuda:3"], 1)
+        parts = Partition.apply(x, ["cuda:0", "cuda:1", "cuda:2", "cuda:3"], 1)
+        parts2 = Partition.apply(self.weight, ["cuda:0", "cuda:1", "cuda:2", "cuda:3"], 0)
 
         futures = []
         with ThreadPoolExecutor() as executor:
             for i in range(len(parts)):
                 futures.append(executor.submit(self.multiply, parts[i], parts2[i]))
         
-        res = 0
+        # manual implementation of reduce
+        # res = 0
+        # for future in futures: 
+        #     res += future.result().to("cuda:0")
+        #     print(res)
+
+        vals = []
         for future in futures:
-            res += future.result()
+            vals.append(future.result())
+
+        res = Reduce.apply(vals, ["cuda:0"])
         
         return res
 
-model = testModel()
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model.to(device)
+def PartitionTest():
+    model = testModelWPartition()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-criterion = nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    criterion = nn.MSELoss()
 
 
-input_tensor = torch.rand((4, 4)).to(device)
-target = torch.rand((4, 4)).to(device)
+    input_tensor = torch.rand((4, 4)).to(device)
+    target = torch.rand((4, 4)).to(device)
 
-for epoch in range(2):
-    optimizer.zero_grad()
+    for epoch in range(20):
+        optimizer.zero_grad()
 
-    output = model(input_tensor)
+        output = model(input_tensor)
 
-    loss = criterion(output, target)
+        loss = criterion(output, target)
 
-    loss.backward()
+        loss.backward()
 
-    optimizer.step()
+        optimizer.step()
+        
+        print(f"Epoch {epoch}, Loss: {loss.item():.4f}")
 
-    # print(f"Epoch {epoch}, Loss: {loss.item():.4f}")
+PartitionTest()
+
+class testModel2(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.weight = nn.Parameter(torch.rand((4, 4)))
+    
+    @staticmethod
+    def multiply(a, b):
+        res = torch.matmul(a, b)
+        return res
+    
+    def forward(self, x):
+        parts = Partition.apply(x, ["cuda:0", "cuda:1", "cuda:2", "cuda:3"], 0)
+        dups = Replicate.apply(self.weight, ["cuda:0", "cuda:1", "cuda:2", "cuda:3"])
+
+        futures = []
+        with ThreadPoolExecutor() as executor:
+            for i in range(len(parts)):
+                futures.append(executor.submit(self.multiply, parts[i], dups[i]))
+        
+        vals = []
+        for future in futures:
+            vals.append(future.result())
+
+        res = Combine.apply(vals, ["cuda:0"], 0)
+        return res
+    
+def Test2():
+    model = testModel2()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    criterion = nn.MSELoss()
+
+
+    input_tensor = torch.rand((4, 4), requires_grad=True).to(device)
+    target = torch.rand((4, 4)).to(device)
+
+    for epoch in range(20):
+        optimizer.zero_grad()
+
+        output = model(input_tensor)
+
+        loss = criterion(output, target)
+
+        loss.backward()
+
+        optimizer.step()
+        
+        print(f"Epoch {epoch}, Loss: {loss.item():.4f}")
+
+
+# Test2()
+
+

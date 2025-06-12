733de24121216f5809a735b29a4b28237a88396d
Ryan <ryanmtle@gmail.com>
2025-06-12 16:49:51 -0700

    parallel tensor attrs

--- pcg/pcg_nodes/pcg_node.py
+++ pcg/pcg_nodes/pcg_node.py
@@ -1,13 +1,15 @@
 import torch
+from pcg.pcg_nodes.parallel_tensor_attrs import *
 
 class PCGNode:
-    def __init__(self, name, parents, data=None):
+    def __init__(self, name, parents, parallel_tensor_attrs: ParallelTensorAttrs, data=None):
         self.name = name
         self.parents = parents
         self.data = []
-        
         self.data.append(data)
-    
+
+        self.parallel_tensor_attrs = parallel_tensor_attrs
+
     def forward(self):
         raise NotImplementedError
 
---
+++ pcg/pcg_nodes/shard_parallel_dim.py
@@ -0,0 +1,5 @@
+class ShardParallelDim:
+    def __init__(self, size: int, degree: int) -> None:
+        self.size = size
+        self.degree = degree
+        
